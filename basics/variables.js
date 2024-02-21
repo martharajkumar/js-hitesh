b776ce305bedb21c46508fa274298b06e9f7f071
martharajkumar <45479237+martharajkumar@users.noreply.github.com>
2024-02-21 11:12:37 +0000

    "learnt variables in js"

---
+++ basics/variables.js
@@ -0,0 +1,37 @@
+const accountId="1433354"
+let accountEmail="xxxxxxx@xxxx.com"
+
+var accountPassword="11222"
+
+accountCity="Hyd"
+
+let accountState
+
+console.log(accountId)
+console.table([accountId,accountEmail,accountPassword,accountCity])
+
+
+//due to block scope and functional scope var is not used 
+
+//Datatypes
+
+// "usestrict" new js version
+
+//alert can't be used here
+
+ let name="rajesh"
+ let age=18
+ let isLoggedIn=false
+
+
+ //string
+ //number
+ //bigint
+ //boolean
+ //null =>type object
+ //undefined
+ //symbol => unique
+
+ //object 
+ console.log(typeof null)
+ console.log(typeof undefined)
